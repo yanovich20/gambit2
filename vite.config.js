05e62f26c10102a1ecb78cc22a442da3b8223da1
Anatoliy <a.yanovich@jdplex.com>
2025-03-03 23:53:51 +0300

    gix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/gambit2",
+  base:"/gambit2/",
   plugins: [vue(),
     tailwindcss()
   ],
