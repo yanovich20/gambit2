83ab96ec16c372fdfe3b9d534d0f73863af815be
Anatoliy <a.yanovich@jdplex.com>
2025-03-03 23:31:20 +0300

    fix

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы деплоите на https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yanovich20/gambit.git master:gh-pages
+git push -f git@github.com:yanovich20/gambit2.git master:gh-pages
 
 cd -
